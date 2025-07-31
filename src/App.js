1dc55a8786dcd1d5d14f6f70ce10a6a1ec8f0cff
AWS_SDP_F_412_ Velvaluri Thejaswini <thejuvelvaluri@gmail.com>
2025-07-31 20:10:50 +0530

    Fix basename for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return (
-   
+    <Router >
 
 
       <div className="App">
@@ -40,7 +40,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    
+    </Router>
   );
 }
 
